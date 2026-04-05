d906c917a7395d35a68bba19c0c60d2ebcc3a5f6
Liban Jama <jliban756@gmail.com>
2026-04-05 18:14:37 +0100

    Completed exercise 3

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,28 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function printReceipt(order) {
+  let total = 0;
+
+  console.log("QTY     ITEM                TOTAL");
+
+  for (let i = 0; i < order.length; i++) {
+    const item = order[i];
+    const itemName = item.itemName;
+    const quantity = item.quantity;
+    const unitPricePence = item.unitPricePence;
+
+    const itemTotal = quantity * unitPricePence;
+    total += itemTotal;
+
+    const paddedItem = itemName.padEnd(20, " ");
+    console.log(
+      quantity + "       " + paddedItem + (itemTotal / 100).toFixed(2)
+    );
+  }
+
+  console.log("\nTotal: " + (total / 100).toFixed(2));
+}
+
+printReceipt(order);
